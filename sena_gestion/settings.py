4dd73708040567b76b253edec9e66af7f2d97fe4
SENA Gestion Developer <sena.gestion@example.com>
2026-03-11 12:18:23 -0500

    Correccion critica de Whitenoise para deploy
    - Agregado whitenoise.runserver_nostatic a INSTALLED_APPS
    - Corregida configuracion de middleware para produccion
    - Whitenoise ahora funciona correctamente con Django
    - Soluciona error de collectstatic en deploy
    - Configuracion optimizada para Heroku/Render/Railway

--- sena_gestion/settings.py
+++ sena_gestion/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',  # Para desarrollo
     'core',
 ]
 
@@ -140,7 +141,16 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     
     # Middleware para archivos estáticos
-    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+    MIDDLEWARE = [
+        'django.middleware.security.SecurityMiddleware',
+        'whitenoise.middleware.WhiteNoiseMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
     
     # Configuración de Whitenoise
     WHITENOISE_USE_FINDERS = True
